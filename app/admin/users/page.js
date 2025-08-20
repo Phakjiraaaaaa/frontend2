bd005a2203874c7edaa09651e6a0d93cdd4742e0
Phakjiraaaaaa <phakjira050349@gmail.com>
2025-08-20 11:53:03 +0700

    vv10.5.5

--- app/admin/users/page.js
+++ app/admin/users/page.js
@@ -20,7 +20,7 @@ export default function Page() {
 
     async function getUsers() {
       try {
-        const res = await fetch("http://itdev.cmtc.ac.th:3000/api/users");
+        const res = await fetch("/api/users");
         if (!res.ok) {
           console.error("Failed to fetch data");
           return;
@@ -53,7 +53,7 @@ export default function Page() {
 
     if (result.isConfirmed) {
       try {
-        const res = await fetch(`http://itdev.cmtc.ac.th:3000/api/users/${id}`, {
+        const res = await fetch(`/api/users/${id}`, {
           method: "DELETE",
           headers: { Accept: "application/json" },
         });
